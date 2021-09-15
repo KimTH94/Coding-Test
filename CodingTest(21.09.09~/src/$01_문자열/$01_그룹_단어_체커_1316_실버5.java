f44b2b85d84776a36b3cf85c7498ad8c07a0b9d6
taehwankim <taehwankim@218.38.137.28>
2021-09-15 21:51:49 +0900

    그룹 단어 체

---
+++ CodingTest(21.09.09~/src/$01_문자열/$01_그룹_단어_체커_1316_실버5.java
@@ -0,0 +1,49 @@
+package $01_문자열;
+
+import java.util.Scanner;
+
+public class $01_그룹_단어_체커_1316_실버5 {
+
+	public static boolean solution(String s) {
+		boolean status = true;
+		
+		boolean[] alphabet = new boolean[26];
+		int prev = -1;
+		
+		for(int i = 0 ; i < s.length() ; i++) {
+			int now = s.charAt(i) - 'a';
+			
+			if(prev != now) {
+				if(alphabet[now]) {
+					status = false;
+					return status;
+				}else {
+					alphabet[now] = true;
+				} 
+			}
+			prev = now;
+		}
+		
+		
+		return status;
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int count = 0;
+		for(int i = 0 ; i < N ; i++) {
+			String s = sc.next();
+			if(solution(s)) {
+				System.out.println(s);
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+		
+		sc.close();
+	}
+}
