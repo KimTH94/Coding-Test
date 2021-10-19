6da627cdb872d641f23de148ec347c688cf0ad7a
KimTH94 <kcc9827@naver.com>
2021-10-19 22:40:28 +0900

    Update $02_ArrayList_Set.java

--- CodingTest(21.09.09~/src/$99_자바/$02_ArrayList_Set.java
+++ CodingTest(21.09.09~/src/$99_자바/$02_ArrayList_Set.java
@@ -18,7 +18,7 @@ public class $02_ArrayList_Set {
 		
 		System.out.println("insert first index \"0\" ==> " + list);	// [0, 1, 2, 3, 4]
 		
-		list.set(0, "A");				// 0번째(처음) 인덱스 "A"로 변환
+		list.set(0, "A");		// 0번째(처음) 인덱스 "A"로 변환
 		list.set(list.size() - 1, "Z");	// 마지막 인덱스를 "Z"로 변환
 		
 		System.out.println("Replace first index \"A\", last index list \"Z\" ==> " + list);	// [A, 1, 2, 3, Z]
