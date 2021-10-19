09897b06123e5e464264cc4b189fcd6b5bcc0ee9
taehwankim <taehwankim@localhost>
2021-10-19 22:39:58 +0900

    2021.10.19 $99.자바 ArrayList Set 주석 추

--- CodingTest(21.09.09~/src/$99_자바/$02_ArrayList_Set.java
+++ CodingTest(21.09.09~/src/$99_자바/$02_ArrayList_Set.java
@@ -12,15 +12,15 @@ public class $02_ArrayList_Set {
 		list.add("3");
 		list.add("4");
 		
-		System.out.println("list ==> " + list);
+		System.out.println("list ==> " + list); // [1, 2, 3, 4]
 		
-		list.add(0, "0");
+		list.add(0, "0"); // "0"을 0번째(처음) 추가 
 		
-		System.out.println("insert first index \"0\" ==> " + list);
+		System.out.println("insert first index \"0\" ==> " + list);	// [0, 1, 2, 3, 4]
 		
-		list.set(0, "A");
-		list.set(list.size() - 1, "Z");
+		list.set(0, "A");				// 0번째(처음) 인덱스 "A"로 변환
+		list.set(list.size() - 1, "Z");	// 마지막 인덱스를 "Z"로 변환
 		
-		System.out.println("replace first index \"A\", last index list \"Z\" ==> " + list);
+		System.out.println("Replace first index \"A\", last index list \"Z\" ==> " + list);	// [A, 1, 2, 3, Z]
 	}
 }
