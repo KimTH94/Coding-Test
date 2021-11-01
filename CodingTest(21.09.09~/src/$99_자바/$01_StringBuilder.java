1e11e43ba5f58ac64f2a4d16463c5bc8b0ebe8eb
taehwankim <taehwankim@localhost>
2021-11-01 23:59:49 +0900

    2021-11-01 빗물 골드5

--- CodingTest(21.09.09~/src/$99_자바/$01_StringBuilder.java
+++ CodingTest(21.09.09~/src/$99_자바/$01_StringBuilder.java
@@ -1,35 +1,24 @@
 package $99_자바;
 
 public class $01_StringBuilder {
-	
-		/*
-		int length() 문자열의 길이를 리턴한다.
-		int capacity()	스트링 버퍼에 저장될 수 있는 문자의 개수인 용량을 얻는다.
-		StringBuffer append(String str) 문자열에 str을 더한다.
-		StringBuffer insert(int offset, String str) 주어진 위치에 문자열을 삽입한다.
-		StringBuffer reverse()	문자열을 거꾸로 만든다.
-		StringBuffer replace(int start, int end, String str) 주어진 위치에 문자열을 주어진 문자열로 대체한다.
-		void setCharAt(int index, char ch)	주어진 인덱스에 있는 문자를 주어진 문자로 바꾼다.
-		String substring(int beginIndex, int endIndex)	원래 문자열에서 beginIndex에서 endIndex까지의 내용으로된 문자열을 리턴
-		String toString()	StringBuffer를 String으로 변환한다.
-	 	*/
+
 	public static void main(String[] args) {
 		
 		StringBuilder sb = new StringBuilder("Default String");
-		System.out.println("sb : " + sb);
-		System.out.println("sb.toString() : " + sb.toString());
-		System.out.println("sb.length() : " + sb.length());
-		System.out.println("sb.capacity() : " + sb.capacity());
-		System.out.println("sb.append(\"!\") : " + sb.append("! "));
-		System.out.println("sb.append(\" \") : " + sb.append(" "));
-		System.out.println("sb.append(\"true\") : " + sb.append(true));
-		System.out.println("sb.reverse() : " + sb.reverse());
+		System.out.println("sb : " + sb);											// sb : Default String
+		System.out.println("sb.toString() : " + sb.toString());						// sb.toString() : Default String
+		System.out.println("sb.length() : " + sb.length());							// sb.length() : 14
+		System.out.println("sb.capacity() : " + sb.capacity());						// sb.capacity() : 30
+		System.out.println("sb.append(\"!\") : " + sb.append("! "));				// sb.append("!") : Default String!
+		System.out.println("sb.append(\" \") : " + sb.append(" "));					// sb.append(" ") : Default String!
+		System.out.println("sb.append(\"true\") : " + sb.append(true));				// sb.append("true") : Default String!  true
+		System.out.println("sb.reverse() : " + sb.reverse());						// sb.reverse() : eurt  !gnirtS tluafeD
 		
 		sb = new StringBuilder("123456");
-		System.out.println("sb : " + sb);
-		System.out.println("sb.replace(0, 1, \"A\") : " + sb.replace(0, 1, "A"));
+		System.out.println("sb : " + sb);											// sb : 123456
+		System.out.println("sb.replace(0, 1, \"A\") : " + sb.replace(0, 1, "A"));	// sb.replace(0, 1, "A") : A23456
 		sb.setCharAt(0, '*');
-		System.out.println("sb.setCharAt(0, \"*\") : " + sb);
-		System.out.println("sb.setCharAt(0, \"*\") : " + sb.substring(0, 2));
+		System.out.println("sb.setCharAt(0, \"*\") : " + sb);						// sb.setCharAt(0, "*") : *23456
+		System.out.println("sb.substring(0, 2) : " + sb.substring(0, 2));			// sb.substring(0, 2) : *2
 	}
 }
--- CodingTest(21.09.09~/src/$99_자바/$03_ArrayList_Sort.java
+++ CodingTest(21.09.09~/src/$99_자바/$03_ArrayList_Sort.java
@@ -9,9 +9,29 @@ public class $03_ArrayList_Sort {
 	public static void main(String[] args) {
 		
 		ArrayList<String> list = new ArrayList<String>();
-		list.add("1");
-		list.add("2");
-		list.add("3");
-		list.add("4");
+
+		list.add("A");
+		list.add("b");
+		list.add("a");
+		list.add("C");
+		list.add("e");
+		
+		System.out.println(list);	// [2, 1, 5, 4, 3]
+		
+		Collections.sort(list);
+		System.out.println(list);	// [1, 2, 3, 4, 5]
+		
+		Collections.sort(list, Collections.reverseOrder());
+		System.out.println(list);	// [5, 4, 3, 2, 1] 
+		
+		list = new ArrayList<String>(Arrays.asList("a", "A", "a", "C", "e"));
+		Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+		System.out.println("대소문자 구분없이 오름차순 : " + list); // [a, A, B, C]
+		// 대소문자 구분없이 내림차순
+		Collections.sort(list, Collections.reverseOrder(String.CASE_INSENSITIVE_ORDER));
+		System.out.println("대소문자 구분없이 내림차순 : " + list); // [C, B, a, A]
+
+		
+
 	}
 }
