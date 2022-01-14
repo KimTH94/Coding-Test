69284271b8f417f3403a10cb2c214b72ad68b60c
taehwankim <taehwankim@localhost>
2022-01-14 23:56:22 +0900

    2022-01-14 누적합 분류 추가 / 구간 합 구하기 4, 11659, 실버3

---
+++ CodingTest(21.09.09~/src/$06_누적합_prefixSum/$01_구간_합_구하기_4_11659_실버3.java
@@ -0,0 +1,19 @@
+package $06_누적합_prefixSum;
+
+import java.util.Scanner;
+
+public class $01_구간_합_구하기_4_11659_실버3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[] arr= new int[N + 1];
+		for(int i = 1 ; i < N ; i++) {
+			
+		}
+		sc.close();
+	}
+}
