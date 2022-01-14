4e64eb3195eb2105a78a60f3b1d9849e12db7ecc
KimTH94 <kcc940602@gmail.com>
2022-01-15 01:58:54 +0900

    2022-01-15 누적합 추가 /구간 합 구하기 4, 11659, 실버3

--- CodingTest(21.09.09~/src/$06_누적합_prefixSum/$01_구간_합_구하기_4_11659_실버3.java
+++ CodingTest(21.09.09~/src/$06_누적합_prefixSum/$01_구간_합_구하기_4_11659_실버3.java
@@ -10,9 +10,22 @@ public class $01_구간_합_구하기_4_11659_실버3 {
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		
-		int[] arr= new int[N + 1];
-		for(int i = 1 ; i < N ; i++) {
+		int[] arr= new int[N];
+		int sum = 0;
+		int[] sumArr = new int[N + 1];
+		sumArr[0] = sum;
+		for(int i = 0 ; i < N ; i++) {
+			arr[i] = sc.nextInt();
+			sum += arr[i];
+			sumArr[i + 1] = sum;
+		}
+		
+		for(int t = 0 ; t < M ; t++) {
+			int start = sc.nextInt();
+			int end   = sc.nextInt();
 			
+			//
+			System.out.println(sumArr[end] - sumArr[start - 1]);
 		}
 		sc.close();
 	}
