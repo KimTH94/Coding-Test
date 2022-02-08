f19f87f9ed94ec70d421b7b09dbd65ce913e7db8
KimTH94 <kcc940602@gmail.com>
2022-02-08 21:41:40 +0900

    2022-02-08 / DP

---
+++ CodingTest(21.09.09~/src/$02_다이나믹_프로그래밍/$06_1로_만들기_1463_실버3.java
@@ -0,0 +1,36 @@
+package $02_다이나믹_프로그래밍;
+
+import java.util.*;
+
+public class $06_1로_만들기_1463_실버3 {
+
+	public static int[] DP;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		DP = new int[N + 1];
+		DP[0] = 0;
+		DP[1] = 0;
+		
+		for(int i = 2 ; i <= N ; i++) {
+			DP[i] = DP[i - 1] + 1;
+			
+			if(i % 2 == 0) {
+				DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+			}
+			
+			if(i % 3 == 0) {
+				DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+			}
+		}
+		
+		System.out.println(DP[N]);
+		
+		sc.close();
+	}
+	
+}
