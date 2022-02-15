718d9e7ba7878c3a929373ad3892f9befc8377ab
KimTH94 <kcc940602@gmail.com>
2022-02-15 22:28:33 +0900

    2022-02-15 / DP 백

---
+++ CodingTest(21.09.09~/src/$02_다이나믹_프로그래밍/$11_파도반_수열_9461_실버3.java
@@ -0,0 +1,33 @@
+package $02_다이나믹_프로그래밍;
+
+import java.util.*;
+
+public class $11_파도반_수열_9461_실버3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		ArrayList<Long> list = new ArrayList<Long>();
+		list.add((long) 1);
+		list.add((long) 1);
+		list.add((long) 1);
+		
+		for(int i = 1 ; i <= 97 ; i += 2) {
+			long temp1 = list.get(i - 1) + list.get(i);
+			long temp2 = list.get(i) + list.get(i + 1);
+			list.add(temp1);
+			list.add(temp2);
+		}
+		
+		for(int i = 0 ; i < num ; i++) {
+			int N = sc.nextInt() - 1;
+			System.out.println(list.get(N));
+		}
+
+		sc.close();
+	}
+
+}
