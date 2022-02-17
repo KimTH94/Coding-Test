c5a8433c1f187567bb815214cee26ce9835a68ce
KimTH94 <kcc940602@gmail.com>
2022-02-17 22:37:28 +0900

    2022-02-17 / 백준 DP

---
+++ CodingTest(21.09.09~/src/$02_다이나믹_프로그래밍/$12_퇴사_14501_실버3.java
@@ -0,0 +1,33 @@
+package $02_다이나믹_프로그래밍;
+
+import java.util.*;
+
+public class $12_퇴사_14501_실버3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] T = new int[N];
+		int[] P = new int[N];
+		
+		for(int i = 0 ; i < N ; i++) {
+			T[i] = sc.nextInt();
+			P[i] = sc.nextInt();
+		}
+		
+		int[] DP = new int[N + 1];
+		for(int i = 0 ; i < N ; i++) {
+			if(i + T[i] <= N) {
+				DP[i + T[i]] = Math.max(DP[i + T[i]], DP[i] + P[i]);
+			}
+            DP[i + 1] = Math.max(DP[i + 1], DP[i]);
+		}
+
+		System.out.println(DP[N]);
+		
+		sc.close();
+	}
+}
