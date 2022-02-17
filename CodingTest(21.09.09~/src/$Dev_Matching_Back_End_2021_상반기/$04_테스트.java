d8dcdd872fa00caf6ab1e676681d7fe127f43d1d
R-THKIM-S <R-THKIM-S@localtest.co.kr>
2022-02-17 12:29:15 +0900

    2022-02-17 / 깃 커밋 테스트

--- CodingTest(21.09.09~/src/$Dev_Matching_Back_End_2021_상반기/$04_테스트.java
+++ CodingTest(21.09.09~/src/$Dev_Matching_Back_End_2021_상반기/$04_테스트.java
@@ -1,5 +1,18 @@
 package $Dev_Matching_Back_End_2021_상반기;
 
+import java.util.Scanner;
+
 public class $04_테스트 {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 0 ; i < N ; i++) {
+			
+		}
+		sc.close();
+	}
 }
